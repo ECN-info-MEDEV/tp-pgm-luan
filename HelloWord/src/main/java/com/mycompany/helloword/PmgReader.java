7d8d57a57b529652901ae612ce3ddc880bd08136
Diego Felipe Lopez Avila <dlopezav@unal.edu.co>
2023-11-30 11:51:31 +0100

    Gui pipe

--- HelloWord/src/main/java/com/mycompany/helloword/PmgReader.java
+++ HelloWord/src/main/java/com/mycompany/helloword/PmgReader.java
@@ -4,57 +4,79 @@
  */
 package com.mycompany.helloword;
 import java.io.*;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 /**
  *
  * @author hudsonteixeira
  */
 
 public class PmgReader {
+    private int[][] img;
 
-public int[][] pmgread(String filePath){
-    try{
-        InputStream fileInputStream = new FileInputStream(filePath);
-        Scanner scan = new Scanner(fileInputStream);
-        // Discard the magic number
-        scan.nextLine();
-        // Discard the comment line
-        scan.nextLine();
-        // Read pic width, height and max value
-        int picWidth = scan.nextInt();
-        int picHeight = scan.nextInt();
-        int maxvalue = scan.nextInt();
+    public void pmgread(String filePath){
+        try{
+            InputStream fileInputStream = new FileInputStream(filePath);
+            Scanner scan = new Scanner(fileInputStream);
+            // Discard the magic number
+            scan.nextLine();
+            // Discard the comment line
+            scan.nextLine();
+            // Read pic width, height and max value
+            int picWidth = scan.nextInt();
+            int picHeight = scan.nextInt();
+            int maxvalue = scan.nextInt();
 
-        fileInputStream.close();
+            fileInputStream.close();
 
-         // Now parse the file as binary data
-         fileInputStream = new FileInputStream(filePath);
-         DataInputStream dis = new DataInputStream(fileInputStream);
+             // Now parse the file as binary data
+             fileInputStream = new FileInputStream(filePath);
+             DataInputStream dis = new DataInputStream(fileInputStream);
 
-         // look for 4 lines (i.e.: the header) and discard them
-         int numnewlines = 4;
-         while (numnewlines > 0) {
-             char c;
-             do {
-                 c = (char)(dis.readUnsignedByte());
-             } while (c != '\n');
-             numnewlines--;
-         }
-
-         // read the image data
-         int[][] data2D = new int[picHeight][picWidth];
-         for (int row = 0; row < picHeight; row++) {
-             for (int col = 0; col < picWidth; col++) {
-                 data2D[row][col] = dis.readUnsignedByte();
-                 System.out.print(data2D[row][col] + " ");
+             // look for 4 lines (i.e.: the header) and discard them
+             int numnewlines = 4;
+             while (numnewlines > 0) {
+                 char c;
+                 do {
+                     c = (char)(dis.readUnsignedByte());
+                 } while (c != '\n');
+                 numnewlines--;
              }
-             System.out.println();
-         }
-         return data2D;
-    } catch (Exception e) {    
-        System.out.println("error ");
-        return null;
+
+             // read the image data
+             int[][] data2D = new int[picHeight][picWidth];
+             for (int row = 0; row < picHeight; row++) {
+                 for (int col = 0; col < picWidth; col++) {
+                     data2D[row][col] = dis.readUnsignedByte();
+                 }
+             }
+             this.img = data2D;
+        } catch (Exception e) {    
+            System.out.println("error ");
+        }
     }
-}
+    
+    public void histogramme(JPanel frame){
+        Map<Integer, Integer> mapHistory = new TreeMap<Integer, Integer>();
+        for (int c = 0; c < img.length; c++) {
+            for (int r = 0; r < img[c].length; r++) {
+                int value = img[c][r];
+                int amount = 0;
+                if (mapHistory.containsKey(value)) {
+                    amount = mapHistory.get(value);
+                    amount++;
+                } else {
+                    amount = 1;
+                }
+                mapHistory.put(value, amount);
+            }
+        }
+        System.out.println(mapHistory.size());
+        frame.add(new JScrollPane(new Graph(mapHistory)));
+        frame.setVisible(true);
+    }
 }
 
